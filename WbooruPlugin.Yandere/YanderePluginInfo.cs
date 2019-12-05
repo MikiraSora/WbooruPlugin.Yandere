0d7eb66c941c087caf39fb5d8d3c3f94348d31c9
Mikira Sora <mikirasora0409@126.com>
2019-12-05 13:25:40 +0800

    add plugin info

---
+++ WbooruPlugin.Yandere/YanderePluginInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wbooru.Kernel.ProgramUpdater;
+using Wbooru.PluginExt;
+using Wbooru.Utils;
+
+namespace WbooruPlugin.Yandere
+{
+    public class YanderePluginInfo : PluginInfo, IPluginUpdatable
+    {
+        public Version CurrentPluginVersion => GetType().Assembly.GetName().Version;
+
+        public override string PluginName => "Yandere Image Source Plugin";
+
+        public override string PluginAuthor => "MikiraSora";
+
+        public override string PluginDescription => "The plugin could provide image and related functions.";
+
+        public IEnumerable<ReleaseInfo> GetReleaseInfoList()
+        {
+            return UpdaterHelper.GetGithubAllReleaseInfoList("");
+        }
+    }
+}
