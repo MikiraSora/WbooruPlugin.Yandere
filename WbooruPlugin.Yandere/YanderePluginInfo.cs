eb1526ae807152dd691c31b3240eb00122e77a6b
Mikira Sora <mikirasora0409@126.com>
2019-12-05 17:27:03 +0800

    change namespace

--- WbooruPlugin.Yandere/YanderePluginInfo.cs
+++ WbooruPlugin.Yandere/YanderePluginInfo.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Wbooru.Kernel.ProgramUpdater;
+using Wbooru.Kernel.Updater;
 using Wbooru.PluginExt;
 using Wbooru.Utils;
 
